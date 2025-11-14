83c7a70d23efc9bfbd8f27c7fd60af99469ed59d
TuXun1999 <617612390@qq.com>
2025-11-14 04:16:50 +0000

    Update train.sh

--- train_ddpm_objects.sh
+++ train_ddpm_objects.sh
@@ -3,5 +3,6 @@ directories=../trained_models
 for dir in "$directories"/*; do
     dir=$(basename ${dir})
     echo "$dir"
-    python3 train_diffusion_policy.py -o $dir -t train_ddpm -g 1 -d ../trained_models
+    python3 train_diffusion_policy.py -o $dir -t train_ddpm -g 1 -d $directories
+    python3 train_diffusion_policy.py -o $dir -t train_ddpm -g 2 -d $directories
 done
